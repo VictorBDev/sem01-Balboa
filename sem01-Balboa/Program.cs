5e5cc545f15d40f142eb4096046060d3772c45b4
victorbdev <victor.balboa.dev@gmail.com>
2024-08-20 17:26:33 -0500

    Agregar archivos de proyecto.

---
+++ sem01-Balboa/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sem01_Balboa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int area = 0;
+            int perimetro = 0;
+
+            Rectangulo rectangulo = new Rectangulo();
+            rectangulo.Base = 10;
+            rectangulo.Altura = 20;
+
+            area = rectangulo.CalcularArea();
+            perimetro = rectangulo.CalcularPerimetro();
+
+            Console.WriteLine("El area es: ");
+            Console.WriteLine(area);
+
+            Console.WriteLine("El perímetro es: ");
+            Console.WriteLine(perimetro);
+
+            Console.WriteLine("Hola Mundo");
+            Console.Read();
+
+            Persona persona = new Persona
+            {
+                Nombres = "Victor",
+                Apellidos = "Balboa",
+            };
+
+        }
+    }
+}
---
+++ sem01-Balboa/Rectangulo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sem01_Balboa
+{
+    internal class Rectangulo
+    {
+        public int Base { get; set; }
+        public int Altura { get; set; }
+
+        public int CalcularArea()
+        {
+            return Base * Altura;
+        }
+
+        public int CalcularPerimetro() //funcion
+        {
+            return 2 * (Base + Altura);
+        }
+    }
+}
